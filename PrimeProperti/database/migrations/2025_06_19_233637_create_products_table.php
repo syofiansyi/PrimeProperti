909ec0f40324546cb92f56af98d23807351f5958
syofiansyi <syofianirwanda21@gmail.com>
2025-06-20 07:26:39 +0700

    s

--- PrimeProperti/database/migrations/2025_06_19_233637_create_products_table.php
+++ PrimeProperti/database/migrations/2025_06_19_233637_create_products_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
       Schema::create('products', function (Blueprint $table) {
         $table->id();
         $table->string('title');
-        $table->string('location');
+        $table->text('location');
         $table->text('description');
-        $table->string('image')->nullable();
+        $table->text('image')->nullable();
         $table->timestamps();
     });
     }
