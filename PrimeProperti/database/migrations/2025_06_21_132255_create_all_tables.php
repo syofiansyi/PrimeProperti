d69a884ddb6579a718fcb1007800f1ba9f82355d
Syofian Irwanda <syofian.irwanda@iconpln.co.id>
2025-07-08 15:04:32 +0700

    s

--- PrimeProperti/database/migrations/2025_06_21_132255_create_all_tables.php
+++ PrimeProperti/database/migrations/2025_06_21_132255_create_all_tables.php
@@ -72,7 +72,7 @@ return new class extends Migration {
        Schema::create('contents', function (Blueprint $table) {
     $table->id();
     $table->string('kategori');
-    $table->json('content');
+    $table->text('content')->nullable();
     $table->timestamps();
 });
 
